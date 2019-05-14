7d12e998adc7c1690a2d51dc7c67b448d4f37409
RalphCuizon <32386939+RalphCuizon@users.noreply.github.com>
2019-05-14 18:30:10 +0200

    up

--- Opdrachten/les06/oefeningFetch/js/script.js
+++ Opdrachten/les06/oefeningFetch/js/script.js
@@ -1,4 +1,29 @@
-;(function(){
-    "use strict";
-    
+(function() {
+  "use strict";
+  window.addEventListener("load", function() {
+    document.getElementById("btnSearch").addEventListener("click", function() {
+      let artist = document.getElementById("artist").value;
+      
+      let url =
+        "https://www.songsterr.com/a/wa/bestMatchForQueryString?s=" +
+        title +
+        "&a=" +
+        artist;
+        function handleError(err) {
+            console.log('Request failed', err)
+        }
+        function handleResponse(resp) {
+          console.log('response status: ', resp.status);
+          return resp.json();
+        }
+        function handleSuccess(data) {
+          console.log('data received: ', data);
+        }
+        
+        fetch(url)
+            .then(handleResponse)
+            .then(handleSuccess)
+            .catch(handleError);
+    });
+  });
 })();
