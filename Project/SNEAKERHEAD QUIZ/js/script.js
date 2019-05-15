ae1f7ba4cc6f77118fba537223c396d85fc01f71
RalphCuizon <32386939+RalphCuizon@users.noreply.github.com>
2019-05-15 20:23:52 +0200

    update

--- Project/SNEAKERHEAD QUIZ/js/script.js
+++ Project/SNEAKERHEAD QUIZ/js/script.js
@@ -1,17 +1,15 @@
 var xhr = new XMLHttpRequest();
 xhr.open("GET", "/question.json");
 xhr.addEventListener("readystatechange", function() {
-        if (xhr.readyState == 4 && xhr.status == 200) {
-          var question = JSON.parse(xhr.responseText);
-          var qList = '<ul class="questions">';
-          for (var i=0; i<question.length; i += 1) {
-           
-            qList += question[i].question + '<br>'  ;
+  if (xhr.readyState == 4 && xhr.status == 200) {
+    var question = JSON.parse(xhr.responseText);
+    var qList = '<ul class="question">';
+    for (var i = 0; i < question.length; i += 1) {
+      qList += question[i].question + "<br>";
+    }
+  }
+  qList += "</ul>";
+  document.getElementById("questions").innerHTML = qList;
+});
 
-          }
-          document.getElementById("question").innerHTML = qList;
-        }
-      });
-     
-      xhr.send();
-    
+xhr.send();
