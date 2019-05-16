20141aefb854c5fb39b0945deb7e8fce62b422ee
RalphCuizon <32386939+RalphCuizon@users.noreply.github.com>
2019-05-16 14:34:34 +0200

    update json

--- Project/SNEAKERHEAD QUIZ/js/script.js
+++ Project/SNEAKERHEAD QUIZ/js/script.js
@@ -1,5 +1,8 @@
 var xhr = new XMLHttpRequest();
-xhr.open("GET", "https://raw.githubusercontent.com/RalphCuizon/MI2-2018-2019/master/Project/SNEAKERHEAD%20QUIZ/question.json");
+xhr.open(
+  "GET",
+  "https://raw.githubusercontent.com/RalphCuizon/MI2-2018-2019/master/Project/SNEAKERHEAD%20QUIZ/question.json"
+);
 xhr.addEventListener("readystatechange", function() {
   if (xhr.readyState == 4 && xhr.status == 200) {
     var question = JSON.parse(xhr.responseText);
@@ -7,15 +10,16 @@ xhr.addEventListener("readystatechange", function() {
     for (var i = 0; i < question.length; i += 1) {
       qList += '<li class="vraag">';
       qList += question[i].question + "<br>";
-      qList += '<input type="radio">';
-      qList += question[i].choice["a"] + "<br>";
-      
-      qList += "</li>";
-      
+      if (question[i].type == "MCSA") {
+        for (var x = 0; x < question[i].answers.length; x += 1) {
+          qList += '<input type="radio">';
+          qList += question[i].answers[x].answer + "<br>";
+          qList += "</li>";
+        }
+      }
+      qList += "</ul>";
     }
-    qList += "</ul>";
   }
-  
   document.getElementById("questions").innerHTML = qList;
 });
 
