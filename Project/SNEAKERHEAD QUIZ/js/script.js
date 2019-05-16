29f0129d70be5ae8c853e8a4260f82f72e5a7fde
RalphCuizon <32386939+RalphCuizon@users.noreply.github.com>
2019-05-16 17:27:51 +0200

    update

--- Project/SNEAKERHEAD QUIZ/js/script.js
+++ Project/SNEAKERHEAD QUIZ/js/script.js
@@ -1,26 +1,48 @@
-var xhr = new XMLHttpRequest();
-xhr.open(
-  "GET",
-  "https://raw.githubusercontent.com/RalphCuizon/MI2-2018-2019/master/Project/SNEAKERHEAD%20QUIZ/question.json"
-);
-xhr.addEventListener("readystatechange", function() {
-  if (xhr.readyState == 4 && xhr.status == 200) {
-    var question = JSON.parse(xhr.responseText);
-    var qList = '<ul class="question">';
-    for (var i = 0; i < question.length; i += 1) {
-      qList += '<li class="vraag">';
-      qList += question[i].question + "<br>";
-      if (question[i].type == "MCSA") {
-        for (var x = 0; x < question[i].answers.length; x += 1) {
-          qList += '<input type="radio">';
-          qList += question[i].answers[x].answer + "<br>";
-          qList += "</li>";
-        }
-      }
-      qList += "</ul>";
-    }
-  }
-  document.getElementById("questions").innerHTML = qList;
-});
+"use strict";
 
-xhr.send();
+let photos = [
+  { url: "/Project/SNEAKERHEAD QUIZ/img/home.jpg" },
+  { url: "/Project/SNEAKERHEAD QUIZ/img/vraag1.jpg" },
+  { url: "/Project/SNEAKERHEAD QUIZ/img/vraag2.jpg" },
+  { url: "/Project/SNEAKERHEAD QUIZ/img/vraag3.jpg" },
+  { url: "/Project/SNEAKERHEAD QUIZ/img/vraag4.jpg" },
+  { url: "/Project/SNEAKERHEAD QUIZ/img/vraag5.jpg" },
+  { url: "/Project/SNEAKERHEAD QUIZ/img/vraag6.jpg" },
+  { url: "/Project/SNEAKERHEAD QUIZ/img/vraag7.jpg" },
+  { url: "/Project/SNEAKERHEAD QUIZ/img/vraag8.jpg" },
+  { url: "/Project/SNEAKERHEAD QUIZ/img/vraag9.jpg" },
+  { url: "/Project/SNEAKERHEAD QUIZ/img/vraag10.jpg" }
+];
+window.addEventListener("load", function() {
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", "question.json");
+  xhr.addEventListener("readystatechange", function() {
+    if (xhr.readyState == 4 && xhr.status == 200) {
+      var question = JSON.parse(xhr.responseText);
+      var qList = '';
+      for (var i = 0; i < question.length; i += 1) {
+        qList += '<ul class="question"><li class="vraag">';
+        qList += question[i].question + "</li>";
+        if (question[i].type == "MCSA") {
+          for (var x = 0; x < question[i].answers.length; x += 1) {
+            qList += `<li><label><input name="answer${i}" type="radio">`;
+            qList += question[i].answers[x].answer + "</label> </li>";
+            
+          }
+        }
+        if (question[i].type == "open") {
+          for (var y = 0; y < question[i].answers.length; y += 1) {
+            qList += '<li><input type="text"></li>';
+
+            
+          }
+
+        }
+        qList += "</ul>";
+      }
+    }
+    document.getElementById("questions").innerHTML = qList;
+  });
+
+  xhr.send();
+});
