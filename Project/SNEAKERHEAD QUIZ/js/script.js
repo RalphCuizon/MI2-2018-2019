50caf48c808f9f84faf5c8477447641722621603
RalphCuizon <32386939+RalphCuizon@users.noreply.github.com>
2019-05-16 12:24:11 +0200

    img
    toevoegen van alle afbeeldingen

--- Project/SNEAKERHEAD QUIZ/js/script.js
+++ Project/SNEAKERHEAD QUIZ/js/script.js
@@ -1,14 +1,18 @@
 var xhr = new XMLHttpRequest();
-xhr.open("GET", "/question.json");
+xhr.open("GET", "https://raw.githubusercontent.com/RalphCuizon/MI2-2018-2019/master/Project/SNEAKERHEAD%20QUIZ/question.json");
 xhr.addEventListener("readystatechange", function() {
   if (xhr.readyState == 4 && xhr.status == 200) {
     var question = JSON.parse(xhr.responseText);
     var qList = '<ul class="question">';
     for (var i = 0; i < question.length; i += 1) {
+      qList += '<li class="vraag">';
       qList += question[i].question + "<br>";
+      qList += "</li>";
+      
     }
+    qList += "</ul>";
   }
-  qList += "</ul>";
+  
   document.getElementById("questions").innerHTML = qList;
 });
 
