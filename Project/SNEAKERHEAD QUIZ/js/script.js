1a48ceb5ba5b9ebf24c784882de6b7320c69871b
RalphCuizon <32386939+RalphCuizon@users.noreply.github.com>
2019-05-21 13:01:23 +0200

    update

--- Project/SNEAKERHEAD QUIZ/js/script.js
+++ Project/SNEAKERHEAD QUIZ/js/script.js
@@ -1,5 +1,6 @@
 "use strict";
 
+// Elke slide heeft een aparte afbeelding
 let photos = [
   { url: "/Project/SNEAKERHEAD QUIZ/img/home.jpg" },
   { url: "/Project/SNEAKERHEAD QUIZ/img/vraag1.jpg" },
@@ -12,14 +13,36 @@ let photos = [
   { url: "/Project/SNEAKERHEAD QUIZ/img/vraag8.jpg" },
   { url: "/Project/SNEAKERHEAD QUIZ/img/vraag9.jpg" },
   { url: "/Project/SNEAKERHEAD QUIZ/img/vraag10.jpg" }
-];
+],
+i = -1;
+
+//Wat er gebeurt wnr de pagina wordt geladen
 window.addEventListener("load", function() {
+  createQuestion();
+
+  //Eerste de class question verbergen
+
+
+  $("#btnStart").click(showQuestion);
+});
+
+function showQuestion() {
+  if(i>=0) {
+    document.querySelectorAll(".question")[i].removeAttribute("id");
+  }
+  i++;
+  document.querySelectorAll(".question")[i].setAttribute("id", "currentQuestion");
+$("#currentQuestion").show("slow");
+}
+
+//Toevoegen van alle vragen en antwoorden
+function createQuestion() {
   var xhr = new XMLHttpRequest();
   xhr.open("GET", "question.json");
   xhr.addEventListener("readystatechange", function() {
     if (xhr.readyState == 4 && xhr.status == 200) {
       var question = JSON.parse(xhr.responseText);
-      var qList = '';
+      var qList = "";
       for (var i = 0; i < question.length; i += 1) {
         qList += '<ul class="question"><li class="vraag">';
         qList += question[i].question + "</li>";
@@ -27,31 +50,19 @@ window.addEventListener("load", function() {
           for (var x = 0; x < question[i].answers.length; x += 1) {
             qList += `<li><label><input name="answer${i}" type="radio">`;
             qList += question[i].answers[x].answer + "</label> </li>";
-            
           }
         }
         if (question[i].type == "open") {
           for (var y = 0; y < question[i].answers.length; y += 1) {
             qList += '<li><input type="text"></li>';
-
-            
           }
-
         }
         qList += "</ul>";
       }
+      document.getElementById("questions").innerHTML = qList;
+      //showQuestion();
     }
-    document.getElementById("questions").innerHTML = qList;
+   
   });
-
   xhr.send();
-
-  /*function hideQuestion() {
-    $("#question").hide("slow");
-  }
-  $("#btnPrevious").click(hideQuestion);
-  /*function showQuestion() {
-    $("#question:first").show("slow");
-  }
-  $("btnNext").click(showQuestion);*/
-});
+}
